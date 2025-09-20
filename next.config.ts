909563c9b9f1dee576409a9f42435e65619a8ea9
flamingo_o <119333749+divyanshu1004@users.noreply.github.com>
2025-09-20 11:28:42 +0530

    Update basePath and assetPrefix for production

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: { unoptimized: true },
-  basePath: isProd ? '/<repo-name>' : '',
-  assetPrefix: isProd ? '/<repo-name>/' : '',
+  basePath: isProd ? '/portfolio.github.io' : '',
+  assetPrefix: isProd ? '/portfolio.github.io/' : '',
   output: 'export'
 };
 
