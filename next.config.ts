cb48a33f87e60dd92d5b82d61196e1b8ee9292bb
flamingo_o <119333749+divyanshu1004@users.noreply.github.com>
2025-09-20 11:40:43 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
-import type { NextConfig } from 'next';
-
-const isProd = process.env.NODE_ENV === 'production';
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   reactStrictMode: true,
   images: { unoptimized: true },
-  basePath: isProd ? '/portfolio.github.io' : '',
-  assetPrefix: isProd ? '/portfolio.github.io/' : '',
   output: 'export'
 };
 
